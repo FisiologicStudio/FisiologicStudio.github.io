015442ee571955c1676f917ba74cb4d78b3f6819
FisiologicStudio <90246415+FisiologicStudio@users.noreply.github.com>
2022-05-10 00:45:00 +0200

    Add files via upload
    Setting the homepage + hamburger menu

--- script/script.js
+++ script/script.js
@@ -1,1 +1,8 @@
-console.log('CIAO');
+
+const handleMenuClick = () => {
+  const navMenu = document.getElementById('nav-menu');
+  const navBtn = document.getElementById('nav-btn')
+  navBtn.classList.toggle('open')
+  navMenu.classList.toggle('open')
+}
+
